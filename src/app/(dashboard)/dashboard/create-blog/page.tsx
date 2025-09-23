3fed4938ef948587f2b99d8f41735d1c75dd32cc
emonpappu17 <emon.pappu.5@gmail.com>
2025-09-23 23:44:45 +0600

    add: CreateBlogForm com

--- src/app/(dashboard)/dashboard/create-blog/page.tsx
+++ src/app/(dashboard)/dashboard/create-blog/page.tsx
@@ -1,9 +1,15 @@
+import CreateBlogForm from "@/components/modules/Blogs/CreateBlogForm";
 import React from "react";
 
 const CreateBlog = () => {
+  // const create = async (data: FormData) => {
+  //   "user server"
+  //   console.log(object);
+  // }
   return (
-    <div>
-      <h1 className="text-center text-xl">Create Blog</h1>
+    <div className="w-full flex justify-center items-center">
+      {/* <h1 className="text-center text-xl">Create Blog</h1> */}
+      <CreateBlogForm></CreateBlogForm>
     </div>
   );
 };
