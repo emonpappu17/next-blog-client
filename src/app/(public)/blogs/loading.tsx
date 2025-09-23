ddb3530a665a2056946c9777484d3bad5e55f5b9
emonpappu17 <emon.pappu.5@gmail.com>
2025-09-23 21:00:11 +0600

    first commit

---
+++ src/app/(public)/blogs/loading.tsx
@@ -0,0 +1,12 @@
+import Loading from '@/components/ui/Loading';
+import React from 'react';
+
+const BlogsLoadingPage   = () => {
+    return (
+        <div className='py-80'>
+            <Loading></Loading>
+        </div>
+    );
+};
+
+export default BlogsLoadingPage ;
--- src/components/modules/Home/Hero.tsx
+++ src/components/modules/Home/Hero.tsx
@@ -40,5 +40,50 @@ export default async function Hero() {
         </section>
       </div>
     </div>
+    // <div>
+   
+
+    //   <div className="min-h-screen w-full bg-[#020617] relative">
+    //     {/* Magenta Orb Grid Background */}
+    //     <div
+    //       className="absolute inset-0 z-0"
+    //       style={{
+    //         background: "#020617",
+    //         backgroundImage: `
+    //     linear-gradient(to right, rgba(71,85,105,0.15) 1px, transparent 1px),
+    //     linear-gradient(to bottom, rgba(71,85,105,0.15) 1px, transparent 1px),
+    //     radial-gradient(circle at 50% 60%, rgba(236,72,153,0.15) 0%, rgba(168,85,247,0.05) 40%, transparent 70%)
+    //   `,
+    //         backgroundSize: "40px 40px, 40px 40px, 100% 100%",
+    //       }}
+    //     />
+    //     {/* Your Content/Components */}
+    //     <section className="relative flex flex-col items-center justify-center text-center py-28 px-6 text-white">
+    //       {/* Headline */}
+    //       <h1 className="text-5xl md:text-7xl font-extrabold tracking-tight max-w-5xl leading-tight">
+    //         Read. Learn. Share. <br className="hidden md:block" />
+    //         Ideas That Shape the Future
+    //       </h1>
+
+    //       {/* Subheadline */}
+    //       <p className="mt-6 text-lg md:text-xl max-w-2xl">
+    //         Discover in-depth articles, tutorials, and thought-provoking stories
+    //         from a global network of creators. Stay updated with fresh insights
+    //         on technology, design, productivity, and personal growth—all in one
+    //         place.
+    //       </p>
+
+    //       {/* CTA */}
+    //       <div className="mt-10 flex flex-col sm:flex-row gap-4">
+    //         <Link
+    //           href="/blogs"
+    //           className="inline-flex items-center justify-center px-8 py-4 font-medium rounded-xl border border-input hover:bg-accent hover:text-accent-foreground transition"
+    //         >
+    //           Explore Blogs
+    //         </Link>
+    //       </div>
+    //     </section>
+    //   </div>
+    // </div>
   );
 }
